5b6878f53ed61cce8d66b81fc6e2fdf0afe55e9b
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-10-08 10:13:24 -0300

    Commit inicial

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 08 Oct 2022 13:12:05 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
